610331103a28c80a6928e8000c7c627030e1a052
w3nder <unktools@gmail.com>
2026-09-25 15:29:20 -0300

    fix(test): imagem do MinIO vem do espelho alpine/minio
    A MinIO despublicou as imagens: quay.io/minio/minio devolve 401 em qualquer
    tag e minio/minio saiu do Docker Hub, o que derrubava na CI todo teste que
    sobe S3 com "access to the requested resource is not authorized". Passa a
    usar alpine/minio:RELEASE.2025-10-15T17-29-55Z, o mesmo espelho dos
    fixtures do sender-vectax; ele roda como usuario minio, entao o servidor
    grava em /home/minio/data.

--- test/gateway_call_test.go
+++ test/gateway_call_test.go
@@ -99,7 +99,7 @@ func TestGatewayInboundCallRecordsAndPublishes(t *testing.T) {
 	conn := startRabbitMQ(t)
 	redisClient := startRedis(t)
 
-	minioContainer, err := minio.Run(ctx, minioImage)
+	minioContainer, err := minio.Run(ctx, minioImage, minioServerOnUserDir)
 	if err != nil {
 		t.Fatalf("failed to start minio container: %v", err)
 	}
